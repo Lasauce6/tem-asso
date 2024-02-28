46395e34104b1a90e7efd8b1ebdee8d20054c639
Raphaël <lasaucisse006@gmail.com>
2024-02-28 18:16:50 +0100

    responsive miscs

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -22,14 +22,14 @@
             </div>
         </section>
 
-        <div class="container mx-auto my-20">
+        <div class="container mx-auto my-20 flex flex-col sm:flex-row">
             <div class="row flex items-center">
                 <div class="column xl-5 place-self-start">
                     <img class="w-full rounded-xl object-cover shadow-xl"
                          src="{{ asset('storage/' . $post->thumbnail) }}" alt="{{ $post->title }}">
                 </div>
 
-                <div class="column xl-7 ">
+                <div class="column xl-7">
                     <article class="s-content__entry entry format-standard">
                         <div class="s-content__media">
 
