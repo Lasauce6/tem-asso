c55e58aa01b11342b8a4a51528003ae46c973616
Raphaël BATICLE <61377616+Lasauce6@users.noreply.github.com>
2026-05-27 12:30:53 +0200

    Refactor scroll down section in index.blade.php and clean up styles.css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -31,13 +31,19 @@
                         </div>
                     </div> <!-- s-intro__content-media -->
 
-                    <div class="s-intro__scroll-down">
+                    <div class="s-intro__scroll-down flex flex-col md:flex-row">
                         <a href="#about" class="smoothscroll">
-                            <div class="scroll-icon">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36" fill="none" stroke="#97b34a" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">&lt;!--!  Atomicons Free 1.00 by @atisalab License - https://atomicons.com/license/ (Icons: CC BY 4.0) Copyright 2021 Atomicons --&gt;<polyline points="7 13 12 18 17 13"></polyline><line x1="12" y1="18" x2="12" y2="6"></line></svg>
+                            <div class="flex mr-2">
+                                <div class="scroll-icon mt-3">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36" fill="none" stroke="#97b34a" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">&lt;!--!  Atomicons Free 1.00 by @atisalab License - https://atomicons.com/license/ (Icons: CC BY 4.0) Copyright 2021 Atomicons --&gt;<polyline points="7 13 12 18 17 13"></polyline><line x1="12" y1="18" x2="12" y2="6"></line></svg>
+                                </div>
                             </div>
-                            <span>Plus d'informations</span>
-                            <a class="btn btn--primary" href="{{ route('nous-soutenir') }}"> Nous soutenir </a>
+                            <div class="flex mr-14">
+                                <span>Plus d'informations</span>
+                            </div>
+                            <div class="hidden md:flex mt-3.5">
+                                <a class="btn btn--primary" href="{{ route('nous-soutenir') }}"> Nous soutenir </a>
+                            </div>
                         </a>
                     </div> <!-- s-intro__scroll-down -->
 
